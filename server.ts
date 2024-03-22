d28487968148717e58b6cea03f078507d9d153ed
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2024-03-22 16:56:54 -0700

    Update cors

--- server.ts
+++ server.ts
@@ -23,10 +23,12 @@ const origins = [
   'https://api.weddingwordsmith.com', 
   'http://www.weddingwordsmith.com', 
   'https://www.weddingwordsmith.com',
-  'http://www.wedding-ws.pro', 
-  'https://www.wedding-ws.pro',
   'http://wedding-ws.pro', 
-  'https://wedding-ws.pro'
+  'https://wedding-ws.pro',
+  'http://api.wedding-ws.pro', 
+  'https://api.wedding-ws.pro',
+  'http://www.wedding-ws.pro', 
+  'https://www.wedding-ws.pro'
 ]
 
 const app = express()
